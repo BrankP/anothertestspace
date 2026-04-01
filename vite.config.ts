0222f9927942235b2d66372162ce183f686ef030
BrankP <itsimple.brad@gmail.com>
2026-04-01 17:20:12 +1100

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   // Relative asset paths make GitHub Pages deployment work without hardcoding a repo name.
-  base: './'
+  base: '/anothertestspace/'
 });
